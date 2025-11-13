0b6b8fc79213b276881e870c3d023bc93eff07a7
aw1nger <aw1nger@yandex.ru>
2025-11-13 20:12:36 +0300

    fix avatar height

--- src/feature/user-info/user-info.tsx
+++ src/feature/user-info/user-info.tsx
@@ -13,7 +13,7 @@ export const UserInfo = () => {
   return (
     <section className="flex container mx-auto flex-col gap-2">
       <div className="flex justify-center w-full">
-        <Avatar className="rounded-lg basis-1/3">
+        <Avatar className="rounded-lg basis-1/3 w-auto h-auto">
           <AvatarImage
             src={user?.initDataUnsafe?.user?.photo_url}
             alt={user?.initDataUnsafe?.user?.username}
