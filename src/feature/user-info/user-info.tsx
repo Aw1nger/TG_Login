b781ef6e27935000d01d60cdb4c32700cbb975b5
aw1nger <aw1nger@yandex.ru>
2025-11-13 20:10:01 +0300

    add container in profile page and fix avatar size

--- src/feature/user-info/user-info.tsx
+++ src/feature/user-info/user-info.tsx
@@ -11,9 +11,9 @@ export const UserInfo = () => {
   const user = useAuth();
 
   return (
-    <section className="flex flex-col gap-2">
-      <div className="flex justify-center">
-        <Avatar className="rounded-lg">
+    <section className="flex container mx-auto flex-col gap-2">
+      <div className="flex justify-center w-full">
+        <Avatar className="rounded-lg basis-1/3">
           <AvatarImage
             src={user?.initDataUnsafe?.user?.photo_url}
             alt={user?.initDataUnsafe?.user?.username}
