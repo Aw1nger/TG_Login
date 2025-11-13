675914c5e44625d31926079888cc6378885c2e1d
aw1nger <aw1nger@yandex.ru>
2025-11-13 19:57:37 +0300

    add waiting to tg ready

--- src/shared/providers/check_tg.tsx
+++ src/shared/providers/check_tg.tsx
@@ -1,31 +1,36 @@
 "use client";
 
-import { useRouter } from "next/navigation";
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState, useEffect, ReactNode } from "react";
+import { Spinner } from "../components/ui/spinner";
 
 const authContext = createContext<TelegramWebApp | null>(null);
-
 
 export const getTelegramWebApp = () => {
   if (typeof window === "undefined") return null;
   return window.Telegram?.WebApp ?? null;
 };
 
-export function AuthProvider({ children }: { children: React.ReactNode }) {
-  const router = useRouter();
+export function AuthProvider({ children }: { children: ReactNode }) {
+  const [tg, setTg] = useState<TelegramWebApp | null>(null);
 
-  const tg = getTelegramWebApp();
+  useEffect(() => {
+    const telegram = getTelegramWebApp();
+    if (!telegram) return;
 
-  if (tg?.ready()) {
-    console.log(tg.initDataUnsafe.user)
+    telegram.ready();
+    setTg(telegram);
+
+  }, []);
+
+  if (!tg) {
     return (
-      <authContext.Provider value={tg}>
-        {children}
-      </authContext.Provider>
-    );
-  } else {
-    console.log("tg is undefind")
+      <div className="flex grow flex-col justify-center items-center">
+        <Spinner scale={2} />
+      </div>)
   }
+
+
+  return <authContext.Provider value={tg}>{children}</authContext.Provider>;
 }
 
 export function useAuth() {
