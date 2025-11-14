59d9ea3b35bbc4abdce2b3153126d5013201c065
aw1nger <aw1nger@yandex.ru>
2025-11-14 13:54:47 +0300

    add awaiting set cookie

--- src/feature/auth_user/auth_user.tsx
+++ src/feature/auth_user/auth_user.tsx
@@ -1,13 +1,33 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 import { setUserCookie } from "@/shared/auth/action";
 import type { User } from "@/shared/auth/user";
+import { Spinner } from "@/shared/components/ui/spinner";
 
 export const AuthUser = ({ user }: { user: User }) => {
-  setUserCookie(user);
+  const [isLoading, setIsLoading] = useState(true);
   const { replace } = useRouter();
-  replace("/profile");
 
-  return null;
+  useEffect(() => {
+    const setUser = async () => {
+      await setUserCookie(user);
+      setIsLoading(false);
+    };
+
+    setUser();
+  }, [user]);
+
+  useEffect(() => {
+    if (!isLoading) {
+      replace("/profile");
+    }
+  }, [isLoading, replace]);
+
+  return (
+    <div className="flex justify-center items-center grow">
+      <Spinner className="size-8" />
+    </div>
+  );
 };
